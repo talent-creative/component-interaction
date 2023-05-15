1eea310ea6b82cb650dfa50daaa219c7cdc8c2e1
Abdelfattah Ragab <fullstackragab@gmail.com>
2023-05-15 06:42:51 +0300

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MissionService } from './mission.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'component-interaction';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroChildComponent } from './hero-child/hero-child.component';
+import { HeroParentComponent } from './hero-parent/hero-parent.component';
+import { NameParentComponent } from './name-parent/name-parent.component';
+import { NameChildComponent } from './name-child/name-child.component';
+import { VersionChildComponent } from './version-child/version-child.component';
+import { VersionParentComponent } from './version-parent/version-parent.component';
+import { VoterComponent } from './voter/voter.component';
+import { VoteTakerComponent } from './vote-taker/vote-taker.component';
+import { CountdownTimerComponent } from './countdown-timer/countdown-timer.component';
+import { CountdownViewChildParentComponent } from './countdown-parent-lv/countdown-parent-lv.component';
+import { MissionControlComponent } from './mission-control/mission-control.component';
+import { AstronautComponent } from './astronaut/astronaut.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroChildComponent,
+    HeroParentComponent,
+    NameParentComponent,
+    NameChildComponent,
+    VersionChildComponent,
+    VersionParentComponent,
+    VoterComponent,
+    VoteTakerComponent,
+    CountdownTimerComponent,
+    CountdownViewChildParentComponent,
+    MissionControlComponent,
+    AstronautComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
